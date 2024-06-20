83ff4b3edb864ac13ca99351bfe37d132355f710
nizar <nizar.saidi.coder@gmail.com>
2024-06-20 17:06:36 +0200

    sudoku_solve can solve basic sudokus, but not hard ones

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 
 /**
  * This function return whether the number is valid in the position (row,col)
@@ -39,8 +40,7 @@ bool is_valid(int grid[9][9], int row, int col, int number)
     }
     return true;
 }
-
-void sudoku_solve(int grid[9][9])
+bool sudoku_solve(int grid[9][9])
 {
     for (int i = 0; i < 9; i++)
     {
@@ -48,20 +48,21 @@ void sudoku_solve(int grid[9][9])
         {
             if (grid[i][j] == 0)
             {
-                int number = 1;
-                bool valid = false;
-                while (number < 10 && !valid)
+                for (int n = 1; n < 10; n++)
                 {
-                    valid = is_valid(grid, i, j, number);
-                    if (valid)
+                    if (is_valid(grid, i, j, n))
                     {
-                        grid[i][j] = number;
+                        grid[i][j] = n;
+                        if (sudoku_solve(grid))
+                            return true;
+                        grid[i][j] = 0;
                     }
-                    number++;
                 }
+                return false;
             }
         }
     }
+    return true;
 }
 void sudoku_print(int grid[9][9])
 {
@@ -85,17 +86,25 @@ void sudoku_print(int grid[9][9])
 int main()
 {
     int sudoku_grid[9][9] = {
-        {4, 0, 0, 0, 0, 0, 8, 0, 5},
-        {0, 3, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 7, 0, 0, 0, 0, 0},
-        {0, 2, 0, 0, 0, 0, 0, 6, 0},
-        {0, 0, 0, 0, 8, 0, 4, 0, 0},
-        {0, 0, 0, 0, 1, 0, 0, 0, 0},
-        {0, 0, 0, 6, 0, 3, 0, 7, 0},
-        {5, 0, 0, 2, 0, 0, 0, 0, 0},
-        {1, 0, 4, 0, 0, 0, 0, 0, 0}};
+        {6, 0, 9, 2, 0, 3, 5, 0, 4},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {8, 7, 0, 4, 0, 9, 0, 0, 7},
+        {0, 0, 7, 0, 5, 0, 9, 0, 3},
+        {0, 0, 0, 0, 5, 0, 0, 0, 0},
+        {4, 0, 6, 0, 8, 0, 1, 0, 5},
+        {0, 0, 3, 8, 0, 1, 6, 0, 0},
+        {0, 5, 0, 0, 4, 9, 2, 7, 0},
+        {5, 4, 0, 0, 9, 2, 7, 0, 8}};
+
     sudoku_print(sudoku_grid);
     printf("\n");
+    clock_t start, end;
+    double cpu_time_used;
+    start = clock();
     sudoku_solve(sudoku_grid);
+    end = clock();
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time taken to solve: %f seconds\n", cpu_time_used);
+
     sudoku_print(sudoku_grid);
 }
